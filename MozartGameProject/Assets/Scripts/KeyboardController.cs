77366e852d74c965d7e3671d4e56627ce88cbb64
DESKTOP-NEG3NVU\Maciej <maciej.celewski@interia.pl>
2018-11-10 16:13:48 +0100

    mozart-game-project-10-11-2018

---
+++ MozartGameProject/Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+    public List<GameObject> keyboardObjects;
+    public List<KeyCode> keysCodes = new List<KeyCode>();
+    public KeyState keyState = KeyState.Up;
+    // 1st string nazwa klawisza 2nd gameobject z przypisanym juz dzwiekiem
+    public Dictionary<KeyCode, GameObject> noteKeys = new Dictionary<KeyCode, GameObject>();
+
+    public enum KeyState
+    {
+        Up, Down, Pressed
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < keyboardObjects.Capacity; i++)
+        {
+            noteKeys.Add(keysCodes[i], keyboardObjects[i]);
+        }
+        if (noteKeys != null)
+        {
+            Debug.Log("Dictionary created:");
+        }
+        else
+        {
+            Debug.Log("Something went wrong:");
+        }
+    }
+    void Update()
+    {
+        DetectKey();
+    }
+    void DetectKey()
+    {
+
+        foreach (var key in keysCodes)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                noteKeys[key].GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
---
+++ MozartGameProject/Assets/Scripts/NoteIndicatorKeys.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using MidiJack;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class NoteIndicatorKeys : MonoBehaviour
+{
+    public int noteNumber;
+    public bool isWhite;
+    public AudioSource audioSource;
+    public KeyState keyState = KeyState.Up;
+
+    public enum KeyState
+    {
+        Up, Down, Pressed
+    }
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        UpdateKeyState();
+    }
+
+    void UpdateKeyState()
+    {
+        if (WasJustReleased())
+        {
+            keyState = KeyState.Up;
+            SetProperColor();
+            PlaySoundOnPress();
+        }
+        if (WasJustPressed())
+        {
+            keyState = KeyState.Down;
+            SetProperColor();
+            PlaySoundOnPress();
+        }
+    }
+
+    private bool WasJustReleased()
+    {
+        return MidiMaster.GetKeyUp(noteNumber) || MidiMaster.GetKey(noteNumber) < 0.01f;
+    }
+
+    private bool WasJustPressed()
+    {
+        if (MidiMaster.GetKey(noteNumber) > 0.5f && keyState == KeyState.Up)
+            return true;
+        return MidiMaster.GetKeyDown(noteNumber);
+    }
+
+    void PlaySoundOnPress()
+    {
+        if (keyState == KeyState.Down)
+        {
+            audioSource.Play(0);
+            Debug.Log("GetKeyDown");
+        }
+    }
+
+    void SetProperColor()
+    {
+        Color color;
+        if (!isWhite)
+            color = (keyState == KeyState.Down) ? Color.red : Color.black;
+        else
+            color = (keyState == KeyState.Down) ? Color.red : Color.white;
+        GetComponent<Renderer>().material.color = color;
+    }
+
+
+}
