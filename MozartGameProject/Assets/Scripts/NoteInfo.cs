cd5b717b5cfc948d659af9c05a82af8b28cd6a93
Karol <karool.dwojak@gmail.com>
2018-11-15 22:57:11 +0100

    sets

---
+++ MozartGameProject/Assets/Scripts/NoteInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInfo : MonoBehaviour
+{
+    public int noteNumber;
+    public float noteLenght;
+    public bool isWhite = false;
+    public GameObject noteBar;
+
+    public int NoteNumber { get; set; }
+    public float NoteLenght { get; set; }
+    public bool IsWhite { get; set; }
+
+    void SearchBaseKey()
+    {
+        noteBar = GameObject.Find(NoteNumber.ToString()).GetComponent<GameObject>();
+    }
+
+    void SetPos()
+    {
+        //gameObject.transform.position.x = noteBar.transform.position.x;
+    }
+}
