6ef7024f7144614d453e76b5701295114d9ef6be
GimpG1 <karool.dwojak@gmail.com>
2018-11-16 17:24:16 +0100

    s

--- MozartGameProject/Assets/Scripts/NoteInfo.cs
+++ MozartGameProject/Assets/Scripts/NoteInfo.cs
@@ -9,10 +9,10 @@ public class NoteInfo : MonoBehaviour
     public int noteNumber;
     public float noteLenght;
     public bool isWhite = false;
-    //public GameObject noteBar;
     public Object midiFile;
     public MidiLoader.Loader load;
 
+    private Dictionary<int, bool> noteDict = new Dictionary<int, bool>();
     public int NoteNumber
     {
         get { return noteNumber;} set { noteNumber = value; }
@@ -28,21 +28,72 @@ public class NoteInfo : MonoBehaviour
         get { return isWhite; } set { isWhite = value; }
     }
 
-    void SearchBaseKey()
-    {
-        //noteBar = GameObject.Find(NoteNumber.ToString()).GetComponent<GameObject>();
-    }
-
     void Start()
     {
         GetMidiFilePath();
+        SetNoteDictionary();
+
+        foreach (var item in noteDict)
+        {
+            Debug.Log(item);
+        }
     }
 
     void GetMidiFilePath()
     {
         string path = Path.GetFullPath(midiFile.name);
-        path += ".mid";
+        //path += ".mid";
         //Debug.Log(path);
-        load = new Loader(path);
+        //load = new Loader(path);
+        FileStream file = new FileStream(midiFile.name , FileMode.Open);
+    }
+
+    void SetNoteDictionary()
+    {
+        for (int i = 48; i < 73; i++)
+        {
+            if (i == 49)
+            {
+               noteDict.Add(i,false);
+            }
+            else if(i == 51)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 54)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 56)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 58)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 61)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 63)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 66)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 68)
+            {
+                noteDict.Add(i, false);
+            }
+            else if (i == 70)
+            {
+                noteDict.Add(i, false);
+            }
+            noteDict.Add(i, false);
+            
+        }
     }
 }
