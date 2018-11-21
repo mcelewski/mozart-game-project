73681a932584dbec6886a6675a5debc644912232
Karol <karool.dwojak@gmail.com>
2018-11-21 20:18:55 +0100

    No specials

--- MozartGameProject/Assets/Scripts/NoteInfo.cs
+++ MozartGameProject/Assets/Scripts/NoteInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using MidiLoader.Events.SystemRealTime;
-using UnityEditor.Experimental.UIElements;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -22,79 +20,67 @@ public class NoteInfo : MonoBehaviour
     }
     
     #region Variables
-    private Dictionary<int, bool> _noteDict = new Dictionary<int, bool>();
+    //private Dictionary<int, bool> _noteDict = new Dictionary<int, bool>();
+    private Dictionary<int, NotesInScene> _noteFull = new Dictionary<int, NotesInScene>();
     public List<Transform> _notePositionList = new List<Transform>();
+
     public GameObject _main;
-
-    public List<NotesInScene> _NotesInScenes = new List<NotesInScene>();
-
-    private bool _notesReaded = false;
     #endregion
-
-    public void Foo()
-    {
-        NotesInScene n = new NotesInScene();
-        n.number = 2;
-        n.isWhite = false;
-        n.xPosition = 2.0f;
-
-        _NotesInScenes.Add(n);
-    }
 
     #region Private Methods
     void Start()
     {
         SetNoteDictionary();
-
-        // Set note pos list
-        if (_main != null && _notesReaded == false)
+        if (_notePositionList == null)
         {
-            var childTransforms = _main.GetComponentsInChildren<Transform>();
-            foreach (var trans in childTransforms)
-            {
-                // 0 elem is parent
-                _notePositionList.Add(trans);
-            }
-
-            _notesReaded = true;
+            GetSceneNotesTransforms();
         }
+        CheckNotesList();
     }
 
     private void SetNoteDictionary()
     {
-        for (int i = 48, j = 1; i < 73; i++, j++)
+        Debug.Log("size: " + _notePositionList.Count);
+        foreach (var item in _notePositionList)
         {
-            if (i == 49 || i == 51 || i == 54 || i == 56 ||
-                i == 58 || i == 61 || i == 63 || i == 66 ||
-                i == 68 || i == 70)
+            NotesInScene singleNote = new NotesInScene();
+            for (int i = 48, j = 1; i < 73; i++, j++)
             {
-                _noteDict.Add(i, false);
+                singleNote.number = i;
+                singleNote.xPosition = item.transform.position.x;
+                if (i == 49 || i == 51 || i == 54 || i == 56 ||
+                    i == 58 || i == 61 || i == 63 || i == 66 ||
+                    i == 68 || i == 70)
+                {
+                    singleNote.isWhite = false;
+                }
+                else
+                {
+                    singleNote.isWhite = true;
+                }
+
+                _noteFull.Add(i, singleNote);
             }
-            else
+        }
+    }
+    private void GetSceneNotesTransforms()
+    {
+        if (_main != null)
+        {
+            var childTransforms = _main.GetComponentsInChildren<Transform>();
+            foreach (var trans in childTransforms)
             {
-                _noteDict.Add(i, true);
+                // At 0 index elem is parent
+                _notePositionList.Add(trans);
             }
+        }
+    }
+    private void CheckNotesList()
+    {
+        foreach (var item in _noteFull)
+        {
+            Debug.Log("Number: " + item.Value.number + " IsWhite?: " + item.Value.isWhite + " PositionX: " + item.Value.xPosition);
         }
     }
     #endregion
-    
-    // no in use need fix
-    public void GetNotesInfo(int search, out int number, out bool color)
-    {
-        // avaiable
-        number = 0;
-        color = false;
-
-        foreach (var item in _noteDict)
-        {
-            
-            if (_noteDict.ContainsKey(search))
-            {
-                // not avaiable
-                number = item.Key;
-                color = item.Value;
-            }
-        }
-    }
-    
 }
