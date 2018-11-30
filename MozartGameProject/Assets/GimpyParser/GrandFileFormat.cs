443000e9f2c2032fc95e0714bcad7dd6596f45d7
GimpG1 <karool.dwojak@gmail.com>
2018-11-30 11:13:20 +0100

    c

--- MozartGameProject/Assets/GimpyParser/GrandFileFormat.cs
+++ MozartGameProject/Assets/GimpyParser/GrandFileFormat.cs
@@ -8,35 +8,49 @@ using UnityEngine;
 public class GrandFileFormat : MonoBehaviour
 {
     // Default file format
-    public MidiFormat fileFormat = MidiFormat.First_Format;
+    private string format;
         
+    
     public enum MidiFormat
     {
-        First_Format,     // Single track
-        Second_Format,    // Multiply track, synchronous
-        Third_Format      // Multiply track, asynchronous
+        Single_MultiChannelTrack,       // Single track
+        OneOrMore_simultaneousTrack,    // Multiply track, synchronous
+        OneOrMore_independentTrack      // Multiply track, asynchronous
     }
-    
+
+    public MidiFormat SetupMidiFormat;
+
     public void DetectFormat(byte[] file)
     {
         // Masks to determine file format
-        byte type;     // 4 bytes
-        byte length;   // 4 bytes determine data length
-        byte data;     // max val = 2 x 4 bytes = 32 bits = 8 589 934 591 max value
-            
+        int data = 0;     // max val = 2 x 4 bytes = 32 bits = 8 589 934 591 max value
+       
         // Search begin of file for data 
         for (int i = 0; i < file.Length + 1; i++ )
         {
-            byte[] tmp = file[i];
-            if (i >= 8)
+            if (0 == file[0])
+                SetupMidiFormat = MidiFormat.Single_MultiChannelTrack;   // midi file
+            else
+                Debug.Log("Non midi format.");                           // non midi file
+            
+            // Chunk type in ASCII
+            if (4 <= i)
             {
-                if (0 == tmp[0])
-                    
+                char[] type = new char[4];
+                
+                type[0] = (char) file[0];
+                type[1] = (char) file[1];
+                type[2] = (char) file[2];
+                type[3] = (char) file[3];
+
+                this.format = new string(type);
+            }
+            
+            // Chunk length determine data length
+            if (4 <= i && 8 <= i)
+            {
+                data = BitConverter.ToInt32(file, 4);
             }
         }
-
-        return true;
     }
-
-    
 }
