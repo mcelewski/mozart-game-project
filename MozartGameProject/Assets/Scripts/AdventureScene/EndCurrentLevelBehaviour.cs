91f7907e39062be171a3b935074b578a5d25dcd9
Karol <karool.dwojak@gmail.com>
2019-01-03 22:18:08 +0100

    Leave level

--- MozartGameProject/Assets/Scripts/AdventureScene/EndCurrentLevelBehaviour.cs
+++ MozartGameProject/Assets/Scripts/AdventureScene/EndCurrentLevelBehaviour.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class EndCurrentLevelBehaviour : MonoBehaviour 
 {
+    //TODO autosave, show summary level, open keyboard scene
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(!IsPlayer(other)) return;   
-        
-        
-        //TODO autosave, show summary level, open keyboard scene
+        if(!IsPlayer(other)) return;
+        AllowToChangeScene(this.gameObject);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if(!IsPlayer(other)) return;
+        DenyToChangeScene(this.gameObject);
     }
     
     void AllowToChangeScene(GameObject sceneToLoad) {
-        Debug.Log("Player can change scene to "+ sceneToLoad);
+        Debug.Log("Player can leave current level");
         SceneMovementController.SetSceneToLoad(sceneToLoad, false);
     }
     
     void DenyToChangeScene(GameObject sceneToLoad) {
-        Debug.Log("Player **cannot** change scene to "+ sceneToLoad);
+        Debug.Log("Player **cannot** leave current level");
         SceneMovementController.SetSceneToLoad(sceneToLoad, true);
     }
     
