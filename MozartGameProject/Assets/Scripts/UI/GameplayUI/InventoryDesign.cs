8bda88140889c9bc576bae87916349cb361dc1b0
Karol <karool.dwojak@gmail.com>
2019-02-08 12:15:42 +0100

    Inventory changes (t450s)

--- MozartGameProject/Assets/Scripts/UI/GameplayUI/InventoryDesign.cs
+++ MozartGameProject/Assets/Scripts/UI/GameplayUI/InventoryDesign.cs
@@ -5,38 +5,67 @@ public class InventoryDesign : MonoBehaviour
 {
     readonly Vector3 startInventoryUIPosition = new Vector3(0,-300,0);
     readonly Vector3 openInventoryPosition = new Vector3(0,300,0);
+    [SerializeField] private InventoryStat inventoryStatus = InventoryStat.Closed;
+    
+    enum InventoryStat
+    {
+        Opened,
+        Closed
+    }
     
     public float smootchTime = 15f;
     public GameObject Inventory;
-    bool opened;
 
     void Awake()
     {
         Inventory.transform.localPosition = startInventoryUIPosition;
     }
 
-    IEnumerator OnClose()
+    void OnClose()
     {
-        yield return new WaitForSeconds(.5f);
-        Inventory.transform.position =
+        //yield return new WaitForSeconds(.5f);
+        Inventory.transform.position =h
             Vector3.Lerp(Inventory.transform.position, startInventoryUIPosition, Time.deltaTime / smootchTime);
-        opened = false;
+        inventoryStatus = InventoryStat.Closed;
     }
 
-    IEnumerator OnOpen()
+    void OnOpen()
     {
-        yield return new WaitForSeconds(.5f);
+        //yield return new WaitForSeconds(.5f);
         Inventory.transform.position = 
             Vector3.Lerp(Inventory.transform.position,openInventoryPosition,Time.deltaTime / smootchTime);
-        opened = true;
+        inventoryStatus = InventoryStat.Opened;
     }
-    
+
+    void OnOpenToward()
+    {
+        Inventory.transform.position = Vector3.MoveTowards(Inventory.transform.position, openInventoryPosition, Time.deltaTime * 10);
+        inventoryStatus = InventoryStat.Opened;
+    }
+    void OnCloseToward()
+    {
+        Inventory.transform.position = Vector3.MoveTowards(Inventory.transform.position, startInventoryUIPosition, Time.deltaTime * 10);
+        inventoryStatus = InventoryStat.Closed;
+    }
+
     public void OpenCloseInventory()
     {
         //TODO fix locations
+        /*
         if (!opened)
             StartCoroutine(OnOpen());
         else
             StartCoroutine(OnClose());
+         */
+        
+        if(!IsOpened())
+            OnOpen();
+        else
+            OnClose();
+    }
+
+    bool IsOpened()
+    {
+        return inventoryStatus == InventoryStat.Opened;
     }
 }
